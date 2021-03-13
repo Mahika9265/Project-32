a187c2528f7f6724ce580f0cbdce385983cfc5a9
Mahika9265 <72756971+Mahika9265@users.noreply.github.com>
2021-03-13 14:13:34 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var Sprite1;
+
+function setup() {
+  createCanvas(800,400);
+  background("grey")
+  createSprite(50, 50, 30, 30);
+
+} 
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
